9cf4fafae3dbe3c5e7a861723b4c28a08fe9f2b0
BChancusi <brandonchancusi@gmail.com>
2019-02-13 14:09:57 +0000

    Heroku config

--- server/app.js
+++ server/app.js
@@ -12,6 +12,17 @@ const app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.use('/reports', reports);
 app.use('/files', files);
 app.use('/fields', fields);
