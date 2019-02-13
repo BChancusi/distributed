d544ddb36e7ae34001f9d88cd786939b8955d976
BChancusi <brandonchancusi@gmail.com>
2019-02-13 21:30:56 +0000

    Heroku config

--- server/database.js
+++ server/database.js
@@ -1,15 +1,35 @@
 const mysql = require('mysql');
 
-const connection = mysql.createPool({
-    connectionLimit : 10,
-    host     : process.env.DATABASE_URL,
-    user     : process.env.DATABASE_USERNAME,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_DATABASE,
-});
 
-connection.on('acquire', function (connection) {
-    console.log('Connection %d acquired', connection.threadId);
-});
+if (process.env.NODE_ENV === 'production') {
+    const connection = mysql.createPool({
+        connectionLimit : 10,
+        host     : process.env.DATABASE_URL,
+        user     : process.env.DATABASE_USERNAME,
+        password : process.env.DATABASE_PASSWORD,
+        database : process.env.DATABASE_DATABASE,
+    });
 
-module.exports = connection;
+    connection.on('acquire', function (connection) {
+        console.log('Connection %d acquired', connection.threadId);
+    });
+
+    module.exports = connection;
+}else{
+    const config = require('./config');
+
+    const connection = mysql.createPool({
+        connectionLimit : 10,
+        host     : config.host,
+        user     : config.user,
+        password : config.password,
+        database : config.database,
+    });
+
+    connection.on('acquire', function (connection) {
+        console.log('Connection %d acquired', connection.threadId);
+    });
+
+    module.exports = connection;
+
+}
