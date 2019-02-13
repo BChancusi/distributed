3e5cdf64027de137e0df673e1120a0758f9cb8e1
BChancusi <brandonchancusi@gmail.com>
2019-02-13 20:53:46 +0000

    Heroku config

--- server/app.js
+++ server/app.js
@@ -7,13 +7,13 @@ const files = require('./files');
 const fields = require('./fields');
 const users = require('./users');
 
+const path = require('path');
+
+
 const app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-
-const path = require('path');
 
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -22,14 +22,16 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     //
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendfile(path.join(__dirname = 'client/build/index.html'));
     })
+}else{
+    //build mode
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/public/index.html'));
+    });
 }
-//build mode
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/public/index.html'));
-});
+
 
 app.use('/reports', reports);
 app.use('/files', files);
