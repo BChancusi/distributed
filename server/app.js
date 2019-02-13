625c56e4859fe909d9dc3641591240c9aa9031d6
BChancusi <brandonchancusi@gmail.com>
2019-02-13 20:57:11 +0000

    Heroku config

--- server/app.js
+++ server/app.js
@@ -7,9 +7,6 @@ const files = require('./files');
 const fields = require('./fields');
 const users = require('./users');
 
-const path = require('path');
-
-
 const app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -19,16 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    //
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
     app.get('/', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
-    })
-}else{
-    //build mode
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/public/index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
