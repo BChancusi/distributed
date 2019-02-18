127019cf74f7d1e5670cd228907eeeea86d483c2
BChancusi <brandonchancusi@gmail.com>
2019-02-18 06:28:27 +0000

    Build config

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve('app', 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve('client', 'build', 'index.html'));
     });
 }
 
