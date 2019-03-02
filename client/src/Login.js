4499db086850d3f483e064053db55681f2e8b585
BChancusi <brandonchancusi@gmail.com>
2019-03-02 17:16:35 +0000

    Login input red on error

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 function Login(props) {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const usernameInput = useRef(null);
+    const passwordInput = useRef(null);
 
     const handleLogin = async (event) => {
         event.preventDefault();
@@ -19,10 +22,15 @@ function Login(props) {
                 throw Error(body.message)
             }
             if (body.express === "details correct") {
+                usernameInput.current.style.backgroundColor = "white";
+                passwordInput.current.style.backgroundColor = "white";
                 setUsername("");
                 setPassword("");
                 props.setLoggedIn("true");
                 localStorage.setItem("loggedIn", "true")
+            }else{
+                usernameInput.current.style.backgroundColor = "red";
+                passwordInput.current.style.backgroundColor = "red";
             }
         });
     };
@@ -40,11 +48,11 @@ function Login(props) {
         <div id="login">
             <form onSubmit={handleLogin}>
                 <label>Username
-                    <input onChange={handleChange} type="text" name="username"
+                    <input onChange={handleChange} type="text" name="username" ref={usernameInput}
                            autoComplete="username" data-testid="username-text"/>
                 </label>
                 <label>Password
-                    <input onChange={handleChange} type="password" name="password"
+                    <input onChange={handleChange} type="password" name="password" ref={passwordInput}
                            autoComplete="current-password" data-testid="password-text"/>
                 </label>
                 <input type="submit" value="Login"/>
